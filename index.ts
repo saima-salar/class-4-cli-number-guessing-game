5faf44a9557e8f9e0491ab5e59eb258a009d29e5
saima-salar <157168170+saima-salar@users.noreply.github.com>
2024-06-24 02:47:17 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+// 1) Computer will generate a number
+
+//2) User input for gussing number
+//3) Compare user input with computer generated number & show result
+
+const randomNumber=Math.floor(Math.random()*10+1);
+// console.log(randomNumber);
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessNumber",
+    type: "number",
+    message: "Please guess a number between 1-10",
+  },
+]);
+if (answers.userGuessNumber === randomNumber) {
+  console.log("Congratulatio! you guessed right number.");
+} else {
+  console.log(" You guess wrong number.");
+}
